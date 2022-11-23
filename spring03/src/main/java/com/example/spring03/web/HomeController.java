ca4a86c12af1db4113b00b1ab76b7aed391e99eb
JMYoon90 <kotis@naver.com>
2022-11-23 15:23:35 +0900

    11.23

--- spring03/src/main/java/com/example/spring03/web/HomeController.java
+++ spring03/src/main/java/com/example/spring03/web/HomeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.spring03.domain.Post;
 import com.example.spring03.service.PostService;
